fba6a36af0aa41bfeaf1087ab14cb8c6cb594e4c
David Brown <75164252+StanleyKubricker@users.noreply.github.com>
2023-12-14 15:38:34 +0000

    Update Linksapptrial.py

--- Linksapptrial.py
+++ Linksapptrial.py
@@ -20,7 +20,7 @@ def estimate_reading_time(text):
     reading_time_min = num_words / wpm
     return round(reading_time_min, 2)
 
-def related_links(url):
+def related_links(title, article_text):
     #Import Modules
     import requests
     from bs4 import BeautifulSoup
@@ -61,22 +61,27 @@ def related_links(url):
         with open('arrays.pkl', 'rb') as f:
             embeddings = pickle.load(f)
 
-        new_url = url
+        #new_url = url
 
-        new_response = requests.get(new_url)
-        new_soup = BeautifulSoup(new_response.text, 'html.parser')
-        new_text = new_soup.find('meta', attrs={'name': 'description'}).get('content')
-        if new_url not in list(articles.keys()):
-            articles[new_url] = new_text
+        #new_response = requests.get(new_url)
+        #new_soup = BeautifulSoup(new_response.text, 'html.parser')
+        #new_text = new_soup.find('meta', attrs={'name': 'description'}).get('content')
+        #if new_url not in list(articles.keys()):
+        #    articles[new_url] = new_text
 
-        new_embedding = get_embeddings(articles[new_url])
+        if title not in list(articles.keys()):
+            articles[title] = article_text
+
+        new_embedding = get_embeddings(articles[title])
+        #new_embedding = get_embeddings(articles[new_url])
 
         # Compute similarity
         similarity_scores = [cosine_similarity(new_embedding, emb) for emb in embeddings]
 
         # Get top 5 similar documents
-        top_5 = sorted(range(len(similarity_scores)), key=lambda i: similarity_scores[i], reverse=True)[:5]
-
+        #top_5 = sorted(range(len(similarity_scores)), key=lambda i: similarity_scores[i], reverse=True)[:5]
+        top_15 = sorted(range(len(similarity_scores)), key=lambda i: similarity_scores[i], reverse=True)[:15]
+        
         embeddings.append(new_embedding)
 
         # Save the list of arrays.
@@ -87,11 +92,17 @@ def related_links(url):
         with open('dict_file.json', 'w') as file:
             json.dump(articles, file)
 
-        top_5_urls = []
+        #top_5_urls = []
 
-        for i in top_5:
-            top_5_urls.append([x for x in articles.keys()][i])
-            top_5_urls = [x for x in top_5_urls if x != new_url]
+        #for i in top_5:
+        #    top_5_urls.append([x for x in articles.keys()][i])
+        #    top_5_urls = [x for x in top_5_urls if x != new_url]
+
+        top_10_titles = []
+
+        for i in top_15:
+            top_10_titles.append([x for x in articles.keys()][i])
+            top_10_titles = [x for x in top_10_titles if x != title][:10]
             
     else:
         article_content = [x for x in articles.values()]
@@ -106,31 +117,42 @@ def related_links(url):
         with open('arrays.pkl', 'rb') as f:
             embeddings = pickle.load(f)
 
-        new_url = url
+        #new_url = url
 
-        new_response = requests.get(new_url)
-        new_soup = BeautifulSoup(new_response.text, 'html.parser')
-        new_text = new_soup.find('meta', attrs={'name': 'description'}).get('content')
-        new_embedding = get_embeddings(articles[new_url])
+        #new_response = requests.get(new_url)
+        #new_soup = BeautifulSoup(new_response.text, 'html.parser')
+        #new_text = new_soup.find('meta', attrs={'name': 'description'}).get('content')
+        #new_embedding = get_embeddings(articles[new_url])
+
+        new_embedding = get_embeddings(articles[title])
 
         # Compute similarity
         similarity_scores = [cosine_similarity(new_embedding, emb) for emb in embeddings]
 
         # Get top 5 similar documents
-        top_10 = sorted(range(len(similarity_scores)), key=lambda i: similarity_scores[i], reverse=True)[:10]
+        #top_10 = sorted(range(len(similarity_scores)), key=lambda i: similarity_scores[i], reverse=True)[:10]
 
-        top_5_urls = []
+        #top_5_urls = []
 
-        for i in top_10:
-            top_5_urls.append([x for x in articles.keys()][i])
-            top_5_urls = [x for x in top_5_urls if x != new_url][:5]
+        #for i in top_10:
+        #    top_5_urls.append([x for x in articles.keys()][i])
+        #    top_5_urls = [x for x in top_5_urls if x != new_url][:5]
+
+        top_15 = sorted(range(len(similarity_scores)), key=lambda i: similarity_scores[i], reverse=True)[:15]
+
+        top_10_titles = []
+
+        for i in top_15:
+            top_10_titles.append([x for x in articles.keys()][i])
+            top_10_titles = [x for x in top_10_titles if x != title][:10]
             
     
-    reading_time = estimate_reading_time(new_text)
+    #reading_time = estimate_reading_time(new_text)
     #print(f"Estimated reading time: {reading_time} minutes")
+    reading_time = estimate_reading_time(article_text)
     
 
-    return reading_time, top_5_urls
+    return reading_time, top_10_titles
 
 # Import Streamlit
 import streamlit as st
@@ -138,18 +160,21 @@ import streamlit as st
 # Title for the app
 st.title('Related Article Links App')
 
-# Take URL input from the user
-input_url = st.text_input('Enter article url:')
+# Take title input from the user
+input_title = st.text_input('Enter article title:')
+
+#Take text input from the user
+input_article_content = st.text_input('Enter article body:')
 
 button_pressed = st.button("Submit")
 
 if button_pressed:
-    time, links = related_links(input_url)
+    time, links = related_links(input_title, str(input_article_content))
     # Display the result
-    st.write(f'The estimated reading time is {time} minutes and the URL links are:')
+    st.write(f'The estimated reading time is {time} minutes and the related titles are:')
     for x in links:
         st.write(x)
 else:
-    st.write('Please enter a URL')
+    st.write('Please enter title and body')
 
 
